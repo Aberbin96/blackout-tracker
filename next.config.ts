24123de76e70b2ad3f3e8ea69c6f6f9c2037da0e
Alejandro Berbin <alejandro.berbin@yesdevs.com>
2026-03-02 12:06:31 +0100

    fix: Type errors in next.config.ts sentry options

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,26 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from "next";
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig: NextConfig = {};
 
 export default withSentryConfig(withNextIntl(nextConfig), {
   org: "ache",
-
   project: "blackout",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
-  widenClientFileUpload: true,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: false,
+
   sourcemaps: {
     disable: true,
   },
 
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // tunnelRoute: "/monitoring",
   webpack: {
     automaticVercelMonitors: true,
